aa24b96c9fba7cd3f89bb0774742d429ddc078a7
Gabriel <extofer@gmail.com>
2012-10-18 21:10:59 -0600

    initializing files

---
+++ BarcodeScanner/src/com/barcodescanner/demo/MyPhoneGapActivity.java
@@ -0,0 +1,12 @@
+package com.barcodescanner.demo;
+
+import com.phonegap.*;
+import android.os.Bundle;
+
+public class MyPhoneGapActivity extends DroidGap {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+}
